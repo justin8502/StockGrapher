b911302960fe511e2fcd29093158895a4e861bcf
justinyijinang <jya005@ucsd.edu>
2016-11-18 12:44:07 -0800

    atr works now

--- File1.R
+++ File1.R
@@ -9,7 +9,9 @@ if(require(quantmod) && require(ggplot2) && require(reshape2)){
   graphics.off()
   rm(list = ls())
   
-  # MACROS
+  # MACROS - Tickers - Vector of tags wanted
+  #          Month - Desired month to start from
+  #          Day - Desired day to start from
   tickers <- c("YHOO","AAPL","IWM","SMH","OIH","XLY")
   month <- 11
   day <- 1
@@ -27,29 +29,45 @@ if(require(quantmod) && require(ggplot2) && require(reshape2)){
   
   # Vector to extract the Open values of Stocks
   # We want the 1st column (the dates)
-  getsub <- c(1)
+  getsubopen <- c(1)
+  getsubatr <- c(1)
   # Setting up vector to extract correctly
   for(i in 0:(numstock-1)) {
-    getsub <- c(getsub, 2+i*6)
+    getsubopen <- c(getsubopen, 2+i*6)
+    if(i == 0){
+      getsubatr <- c(getsubatr, 3)
+    } else {
+      getsubatr <- c(getsubatr, 3+i*4)
+    }
   }
   
   # Plot the Opening values 
   df <- data.frame(Date=index(LargeSet), LargeSet, row.names=NULL)
-  df <- df[, getsub]
+  df <- df[, getsubopen]
   df2 = melt(df, id='Date')
   plot1 <- ggplot(df2, aes(Date, value, color = variable)) + geom_line(size=1)
   
+  # Date to record from
+  datefrom <- paste("2016-", month-1, "-", "0", day, sep = '', collapse='')
+  # Date to record to (default current date)
+  dateto <- paste(Sys.Date(), sep = '', collapse='')
+  # Retrieve data
+  getSymbols(tickers, from=datefrom, to=dateto)
   # Test stuff
   testATR <- do.call(merge, lapply(tickers, function(x) ATR(get(x), 10)))
+
+  row.names(testATR) <- "Date"
+  testATR <- subset(testATR, index(testATR) >= "2016-11-01")
   print(testATR)
   df3 <- data.frame(Date=index(testATR), testATR, row.names=NULL)
-  df3 <- df3[, c(1, 3, 7)]
+  df3 <- df3[, getsubatr]
   df4 = melt(df3, id='Date')
-  ggplot(df4, aes(Date, value, color = variable)) + geom_line(size=1)
+  plot2 <- ggplot(df4, aes(Date, value, color = variable)) + geom_line(size=1)
   
-  summary(plot1)
-
-  plot1
+  # summary(plot1)
+  
+  print(plot1)
+  print(plot2)
 } else {
   print("Load packages failed")
 }
