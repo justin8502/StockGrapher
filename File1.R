65111dd91eab22b2f47cc6049925740827c1e03f
justinyijinang <jya005@ucsd.edu>
2016-11-18 00:12:43 -0800

    Update File1.R

--- File1.R
+++ File1.R
@@ -9,25 +9,39 @@ if(require(quantmod) && require(ggplot2) && require(reshape2)){
   month <- 11
   day <- 1
   
-  getsub <- c(1, 2)
-  
+  # Stores # of stocks we are looking at
   numstock <- length(tickers)
+  # Date to record from
   datefrom <- paste("2016-", month, "-", "0", day, sep = '', collapse='')
+  # Date to record to (default current date)
   dateto <- paste(Sys.Date(), sep = '', collapse='')
+  # Retrieve data
   getSymbols(tickers, from=datefrom, to=dateto)
+  # Condense ticker data into matrix
   LargeSet <- do.call(merge, lapply(tickers, function(x) get(x)))
   
-  for(i in 1:(numstock-1)) {
+  # Vector to extract the Open values of Stocks
+  # We want the 1st column (the dates)
+  getsub <- c(1)
+  # Setting up vector to extract correctly
+  for(i in 0:(numstock-1)) {
     getsub <- c(getsub, 2+i*6)
   }
-   
+  
   # Plot the Opening values 
   df <- data.frame(Date=index(LargeSet), LargeSet, row.names=NULL)
   df <- df[, getsub]
   df2 = melt(df, id='Date')
   ggplot(df2, aes(Date, value, color = variable)) + geom_line(size=1)
   
+  df <- data.frame(Date=index(LargeSet), LargeSet, row.names=NULL)
+  testATR <- do.call(merge, lapply(tickers, function(x) ATR(get(x), 10)))
+  print(testATR)
   
+  plot1 <- ggplot(df2, aes(Date, value, color = variable)) + geom_line(size=1)
+  summary(plot1)
+  
+  plot1
 } else {
   print("Load packages failed")
 }
