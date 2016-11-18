672a0fb23616440fe95d094cbbbbb19d9bfd9c71
justinyijinang <jya005@ucsd.edu>
2016-11-17 23:47:39 -0800

    Update File1.R

--- File1.R
+++ File1.R
@@ -9,7 +9,7 @@ if(require(quantmod) && require(ggplot2) && require(reshape2)){
   month <- 11
   day <- 1
   
-
+  getsub <- c(1, 2)
   
   numstock <- length(tickers)
   datefrom <- paste("2016-", month, "-", "0", day, sep = '', collapse='')
@@ -17,13 +17,17 @@ if(require(quantmod) && require(ggplot2) && require(reshape2)){
   getSymbols(tickers, from=datefrom, to=dateto)
   LargeSet <- do.call(merge, lapply(tickers, function(x) get(x)))
   
-  for(i in 1:numstock) {
-    # print(LargeSet[, (i*6)-5])
+  for(i in 1:(numstock-1)) {
+    getsub <- c(getsub, 2+i*6)
   }
-    
+   
+  # Plot the Opening values 
   df <- data.frame(Date=index(LargeSet), LargeSet, row.names=NULL)
+  df <- df[, getsub]
   df2 = melt(df, id='Date')
   ggplot(df2, aes(Date, value, color = variable)) + geom_line(size=1)
+  
+  
 } else {
   print("Load packages failed")
 }
